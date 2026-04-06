48f11cc3607e1708d6982cf343e22f3d34dde37e
Muhammad Salman <93308098+salmanmuqri@users.noreply.github.com>
2026-04-06 07:21:50 +0530

    Fix Vercel proxy functions export format for API routing

--- api/[...path].js
+++ api/[...path].js
@@ -3,7 +3,7 @@ function getBackendBase() {
   return raw.replace(/\/+$/, '').replace(/\/api$/, '').replace(/\/media$/, '');
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const backendBase = getBackendBase();
   if (!backendBase) {
     return res.status(500).json({
@@ -52,4 +52,4 @@ export default async function handler(req, res) {
       target,
     });
   }
-}
+};
--- media/[...path].js
+++ media/[...path].js
@@ -3,7 +3,7 @@ function getBackendBase() {
   return raw.replace(/\/+$/, '').replace(/\/api$/, '').replace(/\/media$/, '');
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const backendBase = getBackendBase();
   if (!backendBase) {
     return res.status(500).json({
@@ -43,4 +43,4 @@ export default async function handler(req, res) {
       target,
     });
   }
-}
+};
